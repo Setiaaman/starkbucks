cee534e92c112265cf0239e7b2597edc5a390d56
Setiaaman <122644119+Setiaaman@users.noreply.github.com>
2023-01-14 01:54:59 +0530

    Add files via upload

---
+++ Project 2 - Starbucks landing page/index.js
@@ -0,0 +1,8 @@
+function imgslider(anything){
+    document.querySelector(".starbucks").src=anything;
+}
+
+function circlecolorchange(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
